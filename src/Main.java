80e6896e4baeed5496d202f7b7d8c5fb6d45cf31
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-06-18 04:38:57 +0300

    add save CSW

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-/*
-        Toy t1 = new Toy("ro0");
-        Toy t2 = new Toy("rt1");
-        Toy t3 = new Toy("t2");
-        GachaponPlace gpp1 = new GachaponPlace(t1,10,20);
-        GachaponPlace gpp2 = new GachaponPlace(t2,53,1);
-        GachaponPlace gpp3 = new GachaponPlace(t3,1,2);
 
-        Gachapon gp = new Gachapon(new LinkedList<>());
-        gp.addPlace(gpp1);
-        gp.addPlace(gpp2);
-        gp.addPlace(gpp3);
-
-        System.out.println(gp);
-        System.out.println("___________");
-        System.out.println(gp.releaseTheWonToy());
-        System.out.println("___________");
-        System.out.println(gp.releaseTheWonToy());
-        System.out.println("___________");
-        System.out.println(gp);
-
-        SaveFile newFile = new SaveFileCSW();
-        newFile.saveWonToys();
-*/
         CommandParser parserToy = new CommandParserImpl();
         Gachapon gachapon = new Gachapon(new ArrayList<>());
         Controller controller = new Controller(parserToy,gachapon);
